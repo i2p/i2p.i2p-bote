278f612aa4e189ce229c8814e51f0982d49fb311
HungryHobo <HungryHobo@mail.i2p>
2009-12-19 03:27:12 +0000

    rename FindCloseNodesPacketTest to FindClosePeersPacketTest; add PeerListTest

--- test/i2p/bote/AllTests.java
+++ test/i2p/bote/AllTests.java
@@ -24,9 +24,10 @@ package i2p.bote;
 import i2p.bote.folder.IncompleteEmailFolderTest;
 import i2p.bote.packet.EncryptedEmailPacketTest;
 import i2p.bote.packet.I2PBotePacketTest;
+import i2p.bote.packet.PeerListTest;
 import i2p.bote.packet.ResponsePacketTest;
 import i2p.bote.packet.UnencryptedEmailPacketTest;
-import i2p.bote.packet.kademlia.FindCloseNodesPacketTest;
+import i2p.bote.packet.kademlia.FindClosePeersPacketTest;
 import i2p.bote.packet.kademlia.StoreRequestTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -36,7 +37,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { I2PBotePacketTest.class, StoreRequestTest.class, EncryptedEmailPacketTest.class, UnencryptedEmailPacketTest.class,
-	FindCloseNodesPacketTest.class, ResponsePacketTest.class, IncompleteEmailFolderTest.class })
+	FindClosePeersPacketTest.class, PeerListTest.class, ResponsePacketTest.class, IncompleteEmailFolderTest.class })
 public class AllTests {
 
     public static Test suite() {
