e4d317c76d01862b1151aae2c4fb7f5247f84379
HungryHobo <HungryHobo@mail.i2p>
2010-02-09 19:10:04 +0000

    Change K and S to real world values. B is still 1 for now.

--- src/i2p/bote/network/kademlia/KademliaConstants.java
+++ src/i2p/bote/network/kademlia/KademliaConstants.java
@@ -22,8 +22,8 @@
 package i2p.bote.network.kademlia;
 
 public class KademliaConstants {
-    public static final int K = 2;   // Number of redundant storage nodes.
-    public static final int S = 3;   // The size of the sibling list for S/Kademlia.
+    public static final int K = 20;   // Number of redundant storage nodes.
+    public static final int S = 100;   // The size of the sibling list for S/Kademlia.
 //    public static final int B = 5;   // This is the value from the original Kademlia paper.
     public static final int B = 1;
     public static final int ALPHA = 3;   // According to the literature, this is the optimum choice for alpha.
