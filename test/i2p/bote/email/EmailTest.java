88e0f5c0816d725439054b1b0674b074f4336a7b
HungryHobo <HungryHobo@mail.i2p>
2010-06-10 18:30:24 +0000

    More tests

--- test/i2p/bote/email/EmailTest.java
+++ test/i2p/bote/email/EmailTest.java
@@ -22,6 +22,7 @@
 package i2p.bote.email;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import i2p.bote.packet.UnencryptedEmailPacket;
 
@@ -42,11 +43,13 @@ import org.junit.Test;
 public class EmailTest {
     private EmailIdentity[] identities;
     private Email[] emails;
+    private String bccEmailDestination;
+    private EmailIdentity bccIdentity;
 
     @Before
     public void setUp() throws Exception {
-        identities = new EmailIdentity[2];
-        emails = new Email[2];
+        identities = new EmailIdentity[3];
+        emails = new Email[3];
         
         identities[0] = new EmailIdentity("uDl~luJ5RMhTJHIpYIiZUoxyE6xQHcOELDq9yaXELLYnODd88DBClohCfoIKDKOJL4fMBb34mLN42K8ptisCkLiVWliAZl4jiFtaXUGbXtNnZtkCYheelbL5mFyvcuGmmri-smOMZ-ROcio3V18VwQZfBeV-4-LHWpLaa8tqc1B0KVwCr0PVnwihcibut10VdfELhbhLYfI32fHQFTG6hCCZzhhe1jt8Ixl-aTAj2vXaPyJrfWn3M~Md1XsBAuFAQ5EHh0niJgF~CHn~gsRROpVvVZRDL9OAfAGAomnZMFEixnFW6B3Dce-uCTKFP5Jck3n1gP7cgRfVcXsRd4WCvWmzDFlmNMA~fIfscbTseSSke0AzA05sGskqQxNlnLyPFaXSX5OE4szfYz55onKRbgQFIJ-Pru9C8Ejvd7WGocmF6Lz6mtxhnzEl8-~wutAga94XQuRTlGr1kDSsve1hdGMyQ8UrUS~wP3Ke9JjLI7feg~uUI-bB6YvaVsOuVEHCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACQ2~7HQlbJ7oT-fpSot9DUclsw0LGra2Fk86phRsyGriq5uCv1GLGawFTkFvtBkOP");
         identities[0].setPublicName("Max Mustermann");
@@ -61,6 +64,20 @@ public class EmailTest {
         emails[1].setSender(new InternetAddress(identities[1].getPublicName() + " <" + identities[1].getKey() + ">"));
         emails[1].addRecipient(RecipientType.TO, new InternetAddress("Erika Mustermann <m-5~1dZ0MrGdyAWu-C2ecNAB5LCCsHQpeSfjn-r~mqMfNvroR98~BRmReUDmb0la-r-pBHLMtflrJE7aTrGwDTBm5~AJFEm-9SJPZnyGs-ed5pOj4Db65yJml1y1n77qr1~mM4GITl6KuIoxg8YwvPrCIlXe2hiiDCoC-uY9-np9UY>"));
         emails[1].setSubject("Test", "UTF-8");
+        
+        identities[2] = new EmailIdentity("p2Njb7qvettUyfW3-GdMasoi9PIZbszjC-t2y55yFsP-ib4p2wXUlxL2KTofdcQkhOpR24nnZATfol-QQVKS9AOjggfWIkXQcf6dWraE~YIiNyQJ3sa~MixqKixRsSfpsLuFotDPBShvuSwKTIx7~9k0cfbJn98y1HV7VAEz8xhS");
+        identities[2].setPublicName("Lieschen Müller");
+        emails[2] = new Email();
+        emails[2].setSender(new InternetAddress(identities[2].getPublicName() + " <" + identities[2].getKey() + ">"));
+        emails[2].addRecipient(RecipientType.TO, new InternetAddress("E. Kishon <ix4fWGXnWUJRWtTsbFUiNks8BPAsBzEnVuzFErMNikjhtfdkBIiF-XdhmnnHxKRVGTCzGoo8dqoeI2hfLCZxds>"));
+        emails[2].addRecipient(RecipientType.CC, new InternetAddress("R. Sheckley <uXh8-RSBNBVHbLd8ZznhcRcklM2KUHZxYwpEDvBQeyXne0tEiW8IdrT8IA27IjPRSV1k8c4BX9VB0sE7oA0sO3>"));
+        emails[2].addRecipient(RecipientType.CC, new InternetAddress("M. Chrichton <wCJ3MVCHIusW8zMDgCPDMtz3ltLj9MhbkMzwghIJ9lgyIYOnpyJRUeF8q1elT9DAWy893EmKIwWSlyJu5LgG10>"));
+        bccEmailDestination = "lnd40Y0WI2JXWs69EQXLLxpKGrVF-c0xyZiOnAalIhxuHrnvdFqjH-vKWUg6wVooT1etxPCZFzRkWDG8tYMU7O";
+        emails[2].addRecipient(RecipientType.BCC, new InternetAddress("D. Adams <lnd40Y0WI2JXWs69EQXLLxpKGrVF-c0xyZiOnAalIhxuHrnvdFqjH-vKWUg6wVooT1etxPCZFzRkWDG8tYMU7O>"));
+        emails[2].addRecipient(RecipientType.BCC, new InternetAddress("A. Kostner <1-oKfnRkUpgHDAEZww9P~Qmjj7eVkNZ8eOUaYeyiBi9mZrw4CxClXpl2h7iMxRC9kalm9fiiUA2fO6O06A6gXv>"));
+        emails[2].addRecipient(RecipientType.BCC, new InternetAddress("P. Bellemare <n54FM0ASKMQZMB0UZq05EV0TOma6jWTuYA5pyhhvDCxyEBRwsWO5sZQGlcyrjHbB4JxXwjSKhvzRn2ctE79jPA>"));
+        emails[2].setSubject("Test", "UTF-8");
+        bccIdentity = new EmailIdentity("lnd40Y0WI2JXWs69EQXLLxpKGrVF-c0xyZiOnAalIhxuHrnvdFqjH-vKWUg6wVooT1etxPCZFzRkWDG8tYMU7OM7vU0XG79~OcRzXlHsMgfpID4HN71-eOReQm8g0lNGgEJ-OPGDNCT1ti5Zs7bge6hu033Je-ihI2OLCc6WXgK8");
         
         for (Email email: emails) {
             // Make a large email so it is a good test case for testCreateEmailPackets()
@@ -109,6 +126,7 @@ public class EmailTest {
     
     @Test
     public void testCreateEmailPackets() throws MessagingException, IOException, GeneralSecurityException {
+        // convert an email to a byte array, convert back, and compare with the original email
         for (int i=0; i<emails.length; i++) {
             Email email = emails[i];
             EmailIdentity identity = identities[i];
@@ -120,5 +138,30 @@ public class EmailTest {
             Email newEmail = new Email(outputStream.toByteArray());
             assertEquals(email.getContent(), newEmail.getContent());
         }
+    }
+    
+    @Test
+    public void testHeaderRemoval() throws MessagingException, IOException, GeneralSecurityException {
+        Email newEmail;
+        Collection<UnencryptedEmailPacket> packets;
+        ByteArrayOutputStream outputStream;
+        
+        // verify that all BCC addresses are removed when sending to a TO: address
+        packets = emails[2].createEmailPackets(identities[2], null);
+        outputStream = new ByteArrayOutputStream();
+        for (UnencryptedEmailPacket packet: packets)
+            outputStream.write(packet.getContent());
+        newEmail = new Email(outputStream.toByteArray());
+        assertNull("BCC headers were not removed!", newEmail.getHeader("BCC"));
+        assertEquals(3, newEmail.getAllRecipients().length);
+        
+        // verify that the recipient is not removed if it is a BCC: addresses
+        packets = emails[2].createEmailPackets(bccIdentity, bccEmailDestination);   // use the plain email dest because that is what the Email class compares against
+        outputStream = new ByteArrayOutputStream();
+        for (UnencryptedEmailPacket packet: packets)
+            outputStream.write(packet.getContent());
+        newEmail = new Email(outputStream.toByteArray());
+        assertEquals("One BCC header expected!", 1, newEmail.getHeader("BCC").length);
+        assertEquals(4, newEmail.getAllRecipients().length);
     }
 }
