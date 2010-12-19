5ea124b7d2df707c0376721c932b592654eb324e
HungryHobo <HungryHobo@mail.i2p>
2010-12-19 22:22:17 +0000

    Jetty fix

--- src/i2p/bote/web/RequirePasswordTag.java
+++ src/i2p/bote/web/RequirePasswordTag.java
@@ -2,9 +2,11 @@ package i2p.bote.web;
 
 import i2p.bote.io.PasswordException;
 
+import javax.servlet.jsp.el.ELException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 import javax.servlet.jsp.tagext.TryCatchFinally;
 
+@SuppressWarnings("deprecation")   // for javax.servlet.jsp.el.ELException, see below
 public class RequirePasswordTag extends BodyTagSupport implements TryCatchFinally {
     private static final long serialVersionUID = -7546294707936895413L;
     
@@ -17,7 +19,13 @@ public class RequirePasswordTag extends BodyTagSupport implements TryCatchFinall
     
     @Override
     public void doCatch(Throwable t) throws Throwable {
-        if (t instanceof PasswordException || t.getCause() instanceof PasswordException) {
+        boolean isPasswordException = t instanceof PasswordException || t.getCause() instanceof PasswordException;
+        // Special handling of javax.servlet.jsp.el.ELException thrown by Jetty (version 5.1.15, at least):
+        // This exception has a a separate method named getRootCause() which returns the PasswordException
+        // while the regular getCause() method returns null.
+        isPasswordException |= t instanceof ELException && ((ELException)t).getRootCause() instanceof PasswordException;
+        
+        if (isPasswordException) {
             String url = "password.jsp";
             if (forwardUrl != null)
                 url += "?passwordJspForwardUrl=" + forwardUrl;
