058d09d262bdbfa73397fbb78149b96ebdbf06c9
HungryHobo <HungryHobo@mail.i2p>
2011-04-21 04:48:05 +0000

    Pare down flexiprovider some more

--- src/de/flexiprovider/api/exceptions/NoSuchPaddingException.java
+++
@@ -1,9 +0,0 @@
-package de.flexiprovider.api.exceptions;
-
-public class NoSuchPaddingException extends javax.crypto.NoSuchPaddingException {
-
-    public NoSuchPaddingException(String msg) {
-	super(msg);
-    }
-
-}
