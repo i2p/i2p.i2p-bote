a4b284a71c8b476e8f8bcbce57166bc10f80cb1a
HungryHobo <HungryHobo@mail.i2p>
2011-04-25 23:36:22 +0000

    0.2.6

--- src/i2p/bote/I2PBote.java
+++ src/i2p/bote/I2PBote.java
@@ -100,7 +100,7 @@ import net.i2p.util.Log;
  */
 public class I2PBote implements NetworkStatusSource {
     public static final int PROTOCOL_VERSION = 4;
-    private static final String APP_VERSION = "0.2.5";
+    private static final String APP_VERSION = "0.2.6";
     private static final int STARTUP_DELAY = 3;   // the number of minutes to wait before connecting to I2P (this gives the router time to get ready)
     private static volatile I2PBote instance;
     
