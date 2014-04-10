1f9aa61d2f831140e1c8f72b742f16742f66d012
str4d <str4d@mail.i2p>
2014-04-10 20:59:44 +0000

    Upgraded to scrypt 1.4.0

--- WebContent/WEB-INF/lib/decrypt.bat
+++ WebContent/WEB-INF/lib/decrypt.bat
@@ -1,2 +1,2 @@
 REM This script only works if I2P is installed in the standard directory
-@java -cp "%PROGRAMFILES%\\i2p\\lib\\i2p.jar;i2pbote.jar;scrypt-1.3.3.jar" i2p.bote.fileencryption.Decrypt %*
+@java -cp "%PROGRAMFILES%\\i2p\\lib\\i2p.jar;i2pbote.jar;scrypt-1.4.0.jar" i2p.bote.fileencryption.Decrypt %*
--- WebContent/WEB-INF/lib/decrypt.sh
+++ WebContent/WEB-INF/lib/decrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp $I2P/lib/i2p.jar:`dirname $0`/i2pbote.jar:`dirname $0`/scrypt-1.3.3.jar i2p.bote.fileencryption.Decrypt $@
+java -cp $I2P/lib/i2p.jar:`dirname $0`/i2pbote.jar:`dirname $0`/scrypt-1.4.0.jar i2p.bote.fileencryption.Decrypt $@
