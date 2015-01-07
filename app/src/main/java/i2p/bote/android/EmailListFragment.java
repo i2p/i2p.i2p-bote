45e4cba708c3a3e6930a292abc5c3ba6dd9907ac
str4d <str4d@mail.i2p>
2015-01-07 04:31:41 +0000

    Unused import

--- app/src/main/java/i2p/bote/android/EmailListFragment.java
+++ app/src/main/java/i2p/bote/android/EmailListFragment.java
@@ -17,7 +17,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
