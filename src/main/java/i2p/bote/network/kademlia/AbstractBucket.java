0835cfa79332cf83857bf7299164555432fc677f
str4d <str4d@mail.i2p>
2015-11-15 04:57:59 +0000

    Remove unused imports

--- src/main/java/i2p/bote/network/kademlia/AbstractBucket.java
+++ src/main/java/i2p/bote/network/kademlia/AbstractBucket.java
@@ -22,9 +22,7 @@
 package i2p.bote.network.kademlia;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
